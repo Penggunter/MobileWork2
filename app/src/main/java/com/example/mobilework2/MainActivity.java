e2e1028bad6f6617282aca5cf0472b11ff2f0286
shevi <shev.igor2002@yandex.ru>
2023-03-09 20:31:07 +0300

    Commit: initialization of the text component and the image component

--- app/src/main/java/com/example/mobilework2/MainActivity.java
+++ app/src/main/java/com/example/mobilework2/MainActivity.java
@@ -3,6 +3,8 @@ package com.example.mobilework2;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +12,10 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        TextView loadTitle = (TextView)findViewById(R.id.textmain);
+        loadTitle.setText(R.string.helloworld);
+        ImageView loadPicture = (ImageView)findViewById(R.id.kartinka);
+        loadPicture.setImageResource(R.drawable.money);
     }
 }
