d629cbaece645353ed6fec5574f4f95102add429
shevi <shev.igor2002@yandex.ru>
2023-03-09 21:20:19 +0300

    Commit: Log massange. Declarative and Programmatic way.

--- app/src/main/java/com/example/mobilework2/MainActivity.java
+++ app/src/main/java/com/example/mobilework2/MainActivity.java
@@ -2,7 +2,11 @@ package com.example.mobilework2;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.TextView;
 
@@ -17,5 +21,20 @@ public class MainActivity extends AppCompatActivity {
         loadTitle.setText(R.string.helloworld);
         ImageView loadPicture = (ImageView)findViewById(R.id.kartinka);
         loadPicture.setImageResource(R.drawable.money);
+
+
+        // Программный способ
+        Button btnReg = (Button) findViewById(R.id.button2);
+        btnReg.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Log.i("MainActivity", "Data transferred");
+            }
+        });
+    }
+
+    // Декларативный способ
+    public void onClickLog(View view) {
+        Log.i("MainActivity", "Data transferred");
     }
 }
